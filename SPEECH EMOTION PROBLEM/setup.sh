cd33a8bb5e622207452aac4cf86b5ac8730d10a0
Taru <32308101+epicalyx@users.noreply.github.com>
2019-11-05 20:23:59 +0530

    Update setup.sh

--- SPEECH EMOTION PROBLEM/setup.sh
+++ SPEECH EMOTION PROBLEM/setup.sh
@@ -6,11 +6,3 @@ source activate sep
 pip install numpy, xlrd, pandas, matplotlib,librosa,jupyter,tensorflow,keras,scikit-learn,h5py,jsonpickle
 
 echo"SETUP DONE"
-
-echo "PLEASE MAKE SURE DATA FILES, WEIGHT FILES ARE PRESENT IN YOUR SYSTEM"
-echo "TEST FUNCTION RUNNING..."
-
-python test.py
-
-
-echo "TESTING DONE"
