af0b5ee9af5c6a5e5ea69d6d94ccb7069137aea5
Taru <32308101+epicalyx@users.noreply.github.com>
2019-11-05 23:47:59 +0530

    Update setup.sh

--- SPEECH EMOTION PROBLEM/setup.sh
+++ SPEECH EMOTION PROBLEM/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/epicalyx/MIDAS-IIITD-Internship-Task.git ../SPEECH EMOTION PROBLEM
+git clone https://github.com/epicalyx/MIDAS-IIITD-Internship-Task.git
 conda create -n sep python=3.7 -y
 source activate sep
 pip install numpy, xlrd, pandas, matplotlib,librosa,jupyter,tensorflow,keras,scikit-learn,h5py,jsonpickle
